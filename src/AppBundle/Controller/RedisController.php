147bdf54ada500fb44f9954d514b931fdd13ac3a
jacky.yao <jacky.yao@corp.to8to.com>
2020-04-11 22:16:34 +0800

    add socket test

--- src/AppBundle/Controller/RedisController.php
+++ src/AppBundle/Controller/RedisController.php
@@ -4,7 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
+use Symfony\Component\HttpFoundation\Response;
 class RedisController extends Controller
 {
 
@@ -12,11 +12,24 @@ class RedisController extends Controller
      * @Route("/redis"))
      */
 
-    public function redisAction() 
+    public function redisAction()
     {
-        $client = $this->get("snc_redis.default");
-        $client->get("aa");
-        echo "aaa";
-        exit;
+        try {
+            $client = $this->get("snc_redis.default");
+            dump($client->get("aa"));
+        } catch (\Throwable $exception) {
+            $message = $exception->getMessage();
+            $encoding = mb_detect_encoding($message, array("ASCII", 'UTF-8', "GB2312", "GBK", 'BIG5'));
+            if ($encoding != 'UTF-8') {
+                $message = mb_convert_encoding($message, 'UTF-8', $encoding);
+            }
+            dump($message);
+            $this->get('logger')->error($message, (array)$exception->getConnection());
+
+        }
+        return new Response( "<html><body>hello world</body></html>");
     }
+
+
+
 }
