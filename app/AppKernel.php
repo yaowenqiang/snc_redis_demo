721bafb74135d6138f689282a0bd41452e3b604c
jacky.yao <jacky.yao@corp.to8to.com>
2020-04-15 22:15:18 +0800

    add doctrine fixture and doctrine extention bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,9 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            $bundles[] = new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle();
+;
 
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
